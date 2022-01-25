c0615506cceb000af5c16ed46f6f8fbbdbeb89af
Phil <pbrede21@gmail.com>
2022-01-25 10:46:01 -0700

    minor

--- src/remove-user.ts
+++ src/remove-user.ts
@@ -1,11 +1,30 @@
 import type { ServerlessFunctionSignature } from "@twilio-labs/serverless-runtime-types/types";
+import { Twilio } from "twilio";
+
+const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
+const client = new Twilio(ACCOUNT_SID, AUTH_TOKEN);
+
+interface Event {
+  employeeId: string;
+}
 
 export const handler: ServerlessFunctionSignature = async (
   ctx,
-  event,
+  { employeeId }: Event,
   callback
 ) => {
-  console.log("event", event);
+  try {
+    const phoneNumbers = await client.incomingPhoneNumbers.list();
+    const phoneNumber = phoneNumbers.find(
+      (phoneNumber) => phoneNumber.friendlyName === employeeId
+    );
 
-  return callback(null, { yoko: "ono" });
+    if (!phoneNumber) throw Error("No phone number found");
+
+    await client.incomingPhoneNumbers(phoneNumber.sid).remove();
+
+    return callback(null, { phoneNumber });
+  } catch (error) {
+    return callback(error);
+  }
 };
